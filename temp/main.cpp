06c6edd2c747ffa9c2c8d7521452907ee0fd2d41
take32457 <take32457@gmail.com>
2022-03-07 15:08:43 +0900

    ファイルを開いていない状態で名前を付けて保存をした場合、保存されないバグを修正

--- temp/main.cpp
+++ temp/main.cpp
@@ -220,20 +220,14 @@ void commandMode()
 }
 int saveToFile(const char* fileName) 
 {
-	FILE* fp = fopen(fileName, "r+");
-	if (fp == NULL) {
-		fp = fopen(fileName, "w");
-		fclose(fp);
+	FILE* fp = fopen(fileName, "w");
+	E_ROW* temp = E.prow->next;
+	while (temp->next != NULL) {
+		if (temp->next->next == NULL)temp->pChars[strlen(temp->pChars) - 1] = 0;
+		fputs(temp->pChars, fp);
+		temp = temp->next;
 	}
-	else {
-		E_ROW* temp = E.prow->next;
-		while (temp->next != NULL) {
-			if (temp->next->next == NULL)temp->pChars[strlen(temp->pChars) - 1] = 0;
-			fputs(temp->pChars, fp);
-			temp = temp->next;
-		}
-		fclose(fp);
-	}
+	fclose(fp);
 	return 0;
 }
 int loadFromFile(const char* fileName)
